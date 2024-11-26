9945bdb9a79e384801d784f0dc7060f6e70421ac
Lukas Dornstaedter <34348697+lukas-dornstaedter@users.noreply.github.com>
2024-11-26 16:16:24 +0100

    Signup Form

---
+++ signup.js
@@ -0,0 +1,28 @@
+import { supabase } from './supabaseConfig.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('signup-form').addEventListener('submit', async (e) => {
+        e.preventDefault();
+        let formEmail = document.getElementById('signup-email');
+        let formPassword = document.getElementById('signup-password');
+        let alertText = document.getElementById('alert-text');
+
+        const { data, error } = await supabase.auth.signUp({
+            email: formEmail.value,
+            password: formPassword.value,
+          })
+        
+        console.log(data)
+        console.log(error)
+        if(error == null){
+            formEmail.value = '';
+            formPassword.value = '';
+            alertText.innerHTML = 'Signup sucessful. <a href="/login.html">Click here to login</a>.'
+
+        } else {
+            alertText.innerText = error;
+            alertText.style.color = red;
+        }
+    });
+});
+
