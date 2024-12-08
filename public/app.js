51d443fe18ce6f835fffe4f69d36006a333fac78
Lukas Dornstaedter <34348697+lukas-dornstaedter@users.noreply.github.com>
2024-12-08 10:57:08 +0100

    Add progress circle on dashboard

--- public/app.js
+++ public/app.js
@@ -44,18 +44,18 @@ document.addEventListener('DOMContentLoaded', () => {
     getGoals().then((data) => {
         window.goals = data;
         renderGoals()
+        renderProgressBars();
     });
 });
 
 async function getGoals() {
-    const { data, error } = await supabase.from('goal').select();
-
-    if (error == null) {
-        return data;
-    } else {
-        console.error('Error fetching goals:', error);
-        return [];
+    const { data, error } = await supabase.rpc('get_goal_progress');
+    if (error) {
+      console.error('Error fetching goal progress:', error);
+      return;
     }
+    return data;
+
 }
 
 async function createGoal() {
@@ -79,20 +79,56 @@ async function checkIfLoggedIn() {
     }
   }
 
+  function setDonutProgress(id, value) {
+    const progressBar = document.querySelector(`#${id}`);
+    if (!progressBar) {
+      console.error(`Progress bar with ID "${id}" not found.`);
+      return;
+    }
+  
+    const circle = progressBar.querySelector('.donut-progress-fill');
+    const progressText = progressBar.querySelector('.donut-text');
+  
+    const radius = 15;
+    const circumference = 2 * Math.PI * radius;
+  
+    const progress = Math.min(Math.max(value, 0), 100);
+  
+    const offset = circumference - (progress / 100) * circumference;
+  
+    circle.style.strokeDashoffset = offset;
+    progressText.textContent = `${Math.round(progress)}%`;
+  }
+
 function renderGoals(){
     const goalList = document.getElementsByClassName('goal-list')[0];
     goalList.innerHTML = '';
     window.goals.forEach((goal)=>{
         const goalItem = document.createElement('a')
-        goalItem.href = 'goal.html?goalid=' + goal.id
+        goalItem.href = 'goal.html?goalid=' + goal.goal_id
         goalItem.classList.add('goal-item');
         goalItem.innerHTML = `
                         <h3>${goal.name}</h3>
                         <div>
                             <p>${goal.frequency} time per ${goal.frequency_type}</p>
+                            <div class="donut-progress" id="progress-${goal.goal_id}">
+                                <svg class="donut" viewBox="0 0 36 36">
+                                <circle class="donut-bg" cx="18" cy="18" r="15"></circle>
+                                <circle class="donut-progress-fill" cx="18" cy="18" r="15"></circle>
+                                </svg>
+                                <div class="donut-text">0%</div>
+                            </div>
                         </div>
         `;
         goalList.appendChild(goalItem);
     })
 }
 
+function renderProgressBars(){
+    window.goals.forEach((goal)=>{
+        setTimeout(() => {
+            setDonutProgress(`progress-${goal.goal_id}`,goal.progress_percentage);
+          }, 100);
+        
+    })
+}
